53df8ecee7438bef796c629a261ca3059d914c1e
Sean Morrison <seanmorrison112358@gmail.com>
2015-10-27 15:00:03 -0700

    Change sample data to space descriptions; add location and allowed directions; Change views to display space info

--- index.js
+++ index.js
@@ -2,14 +2,19 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 
+// mongo setup
 var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
 var ObjectId = require('mongodb').ObjectID;
 var url = 'mongodb://localhost:27017/advent';
 MongoClient.connect(url, function(err, db) {
-  assert.equal(null, err);
-  console.log("Connected correctly to server.");
-  db.close();
+  if (err) {
+  	console.log('Unable to connect to the mongoDB server. Error:', err);
+  } else {
+	  assert.equal(null, err);
+	  console.log("Connected correctly to server.");
+	  db.close();
+	}
 });
 
 app.get('/', function (req, res) {
@@ -18,9 +23,8 @@ app.get('/', function (req, res) {
 app.use(express.static(__dirname + '/client'));
 
 var server = app.listen(3000, function () {
-  var host = server.address().address;
+ 	var host = server.address().address;
   var port = server.address().port;
-
   console.log('Example app listening at http://%s:%s', host, port);
 });
 
