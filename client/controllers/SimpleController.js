f1bc232d49ddbd874d5811fd2a2eca38162e3433
Sean Morrison <seanmorrison112358@gmail.com>
2015-11-03 13:23:58 -0800

    Add fonts and images. Add a bunch of styling for app and landing page, including the ever popular text + transparency sliding over fixed background image. Spot-checked mobile rendering (not consistent across all devices yet). Edit Landing page text.

--- client/controllers/SimpleController.js
+++ client/controllers/SimpleController.js
@@ -1,8 +1,5 @@
 var controllers = {};
 controllers.SimpleController = function($scope, $route, $location, simpleFactory) {
-	$scope.background1 = {
-		background: 'url(../images/Background-1.jpg)'
-	};
 	$scope.space = [];
 	var request = $location.url();
 	init();
